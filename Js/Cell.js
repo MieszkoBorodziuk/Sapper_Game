336ea358ce2edebfae05bf19882018b6d554a445
Mieszko Borodziuk <mieszko.borodziuk@gmail.com>
2020-09-12 14:57:02 +0200

    generating board squares

--- Js/Cell.js
+++ Js/Cell.js
@@ -1,12 +1,31 @@
-export class Cell {
-    constructor(x,y){
+import { UI } from './UI.js'
+
+export class Cell extends UI {
+    constructor(x, y) {
+        super();
         this.x = x;
         this.y = y;
         this.value = 0;
         this.isMine = false;
         this.isReveal = false;
         this.isFlag = false;
-        this.selector = `[data-x="${this.x}"][data-y="${this.y}]`
+        this.selector = `[data-x="${this.x}"][data-y="${this.y}]`;
         this.element = null;
+    }
+
+    createElement() {
+        const element = `<div class="cell border border--concave" data-cell data-x="${this.x}" data-y="${this.y}"></div>`;
+        return element;
+    }
+    
+    revealCell() {
+        this.isReveal = true;
+        this.element.classList.remove('border--concave');
+        this.element.classList.add('border--revealed');
+    }
+
+    toogleFlag() {
+        this.isFlagged = !this.isFlagged;
+        this.element.classList.toogle('cell--is-flag');
     }
 }
--- Js/Game.js
+++ Js/Game.js
@@ -1,6 +1,7 @@
-import Cell from './Cell.js'
+import { Cell } from './Cell.js';
+import { UI } from './UI.js';
 
-class Game {
+class Game extends UI {
     #config = {
         easy: {
             rows: 8,
@@ -25,18 +26,39 @@ class Game {
 
 
     #cells = [];
+    #cellsElements = null;
+
+    #board = null;
 
     initializeGame() {
-        this.#newGame()
+        this.#handleElements();
+        this.#newGame();
     }
+
 
     #newGame(rows = this.#config.easy.rows, cols = this.#config.easy.cols, mines = this.#config.easy.mines) {
         this.#numberOfRows = rows;
         this.#numberOfCols = cols;
         this.#numberOfMines = mines;
 
+        this.#setStyles();
         this.#generateCells();
+        this.#renderBoard();
+
+        this.#cellsElements = this.getElements(this.UISelectors.cell);
+
+        this.#addCellsEventListeners();
     }
+    #handleElements() {
+        this.#board = this.getElement(this.UISelectors.board)
+    }
+    #addCellsEventListeners() {
+        this.#cellsElements.forEach((element) => {
+          element.addEventListener('click', this.#handleCellClick);
+          element.addEventListener('contextmenu', this.#handleCellContextMenu);
+        });
+      }
+    
 
     #generateCells() {
         for (let row = 0; row < this.#numberOfRows; row++) {
@@ -44,6 +66,35 @@ class Game {
             for (let col = 0; col < this.#numberOfCols; col++)
                 this.#cells[row].push(new Cell(col, row));
         }
+    }
+    #renderBoard() {
+        this.#cells.flat().forEach((cell) => {
+            this.#board.insertAdjacentHTML('beforeend', cell.createElement());
+            cell.element = cell.getElement(cell.selector);
+        });
+    }
+
+    #handleCellClick = (e) => {
+        const target = e.target;
+        const rowIndex = parseInt(target.getAttribute('data-y'), 10);
+        const colIndex = parseInt(target.getAttribute('data-x'), 10);
+        this.#cells[rowIndex][colIndex].revealCell();
+        
+    }
+    #setStyles() {
+        document.documentElement.style.setProperty('--cells-in-row', this.#numberOfCols);
+    }
+    #handleCellContextMenu = (e) =>{
+        e.preventDefault();
+        const target = e.target;
+        const rowIndex = parseInt(target.getAttribute('data-y'), 10);
+        const colIndex = parseInt(target.getAttribute('data-x'), 10);
+        this.#cells[rowIndex][colIndex].revealCell();
+
+        const cell = this.#cells[rowIndex][colIndex]
+
+        if(cell.isReveal) return;
+        cell.toggleFlag();
     }
 }
 
